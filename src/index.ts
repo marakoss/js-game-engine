88de3accdea30cb22a04e85e61188ed945505d4b
Marek Králík <marakoss@gmail.com>
2022-09-30 22:07:21 +0200

    Migrate to typescript

--- src/index.ts
+++ src/index.ts
@@ -1,46 +1,45 @@
 const chat = document.getElementById("console");
 const form = document.getElementById("form");
 const input = document.getElementById("command");
-const saveGameFile = 'myGame';
+const saveGameFile = "myGame";
 loadGame();
 
 const historyTypeEnum = Object.freeze({
-  COMMAND: Symbol('command'),
-  RESPONSE: Symbol('response')
+  COMMAND: Symbol("command"),
+  RESPONSE: Symbol("response"),
 });
 
-form
-  .addEventListener("submit", function(event) {
-    event.preventDefault();
+form.addEventListener("submit", function (event) {
+  event.preventDefault();
 
-    const text = readInput();
+  const text = readInput();
 
-    gameState.history.push({
-      type: historyTypeEnum.COMMAND,
-      text: text,
-      data: createCommand(text),
-      time: new Date().getTime()
-    })
-
-    gameState.history.push({
-      type: historyTypeEnum.RESPONSE,
-      text: getResponse(),
-      time: new Date().getTime()
-    })
-
-    clearInput();
-    writeAllNewMessagesToChat();
-    saveGame();
+  gameState.history.push({
+    type: historyTypeEnum.COMMAND,
+    text: text,
+    data: createCommand(text),
+    time: new Date().getTime(),
   });
 
+  gameState.history.push({
+    type: historyTypeEnum.RESPONSE,
+    text: getResponse(),
+    time: new Date().getTime(),
+  });
+
+  clearInput();
+  writeAllNewMessagesToChat();
+  saveGame();
+});
+
 function saveGame() {
-  if (!isLocalStorageAvailable()) return
+  if (!isLocalStorageAvailable()) return;
 
   const gameData = {
-    gameState: gameState
-  }
+    gameState: gameState,
+  };
 
-  localStorage.setItem(saveGameFile, JSON.stringify(gameData))
+  localStorage.setItem(saveGameFile, JSON.stringify(gameData));
 }
 
 function loadGame() {
@@ -54,20 +53,21 @@ function loadGame() {
 }
 
 function isLocalStorageAvailable() {
-  var test = 'test';
+  var test = "test";
   try {
     localStorage.setItem(test, test);
     localStorage.removeItem(test);
     return true;
   } catch (e) {
-    console.warn('Cannot store/load gameData');
-		return false;
+    console.warn("Cannot store/load gameData");
+    return false;
   }
 }
 
-const levenshteinDistance = (str1 = '', str2 = '') => {
-  const track = Array(str2.length + 1).fill(null).map(() =>
-    Array(str1.length + 1).fill(null));
+const levenshteinDistance = (str1 = "", str2 = "") => {
+  const track = Array(str2.length + 1)
+    .fill(null)
+    .map(() => Array(str1.length + 1).fill(null));
   for (let i = 0; i <= str1.length; i += 1) {
     track[0][i] = i;
   }
@@ -80,7 +80,7 @@ const levenshteinDistance = (str1 = '', str2 = '') => {
       track[j][i] = Math.min(
         track[j][i - 1] + 1, // deletion
         track[j - 1][i] + 1, // insertion
-        track[j - 1][i - 1] + indicator, // substitution
+        track[j - 1][i - 1] + indicator // substitution
       );
     }
   }
@@ -90,19 +90,19 @@ const levenshteinDistance = (str1 = '', str2 = '') => {
 function getGuessedCommand(text) {
   var lowest = 1000;
   var intent = null;
-  const words = text.split(' ');
-  words.forEach(word => {
+  const words = text.split(" ");
+  words.forEach((word) => {
     const commandKeys = Object.keys(commandAliases);
-    commandKeys.forEach(key => {
-      commandAliases[key].forEach(alias => {
+    commandKeys.forEach((key) => {
+      commandAliases[key].forEach((alias) => {
         const distance = levenshteinDistance(alias, word);
         if (distance < lowest) {
           lowest = distance;
           intent = key;
         }
       });
-    })
-  })
+    });
+  });
   //console.log(intent);
   return intent.toLowerCase();
 }
@@ -111,7 +111,7 @@ function createCommand(text) {
   return {
     name: getGuessedCommand(text),
     originalInput: text,
-    transformedInput: text.toLowerCase().trim()
+    transformedInput: text.toLowerCase().trim(),
   };
 }
 
@@ -125,8 +125,9 @@ function clearInput() {
 
 function writeAllNewMessagesToChat() {
   while (gameState.messageCounter < gameState.history.length) {
-    const div = document.createElement('div');
-    div.className = gameState.history[gameState.messageCounter].type.description;
+    const div = document.createElement("div");
+    div.className =
+      gameState.history[gameState.messageCounter].type.description;
     div.innerHTML = gameState.history[gameState.messageCounter].text;
     chat.appendChild(div);
     gameState.messageCounter++;
@@ -136,49 +137,52 @@ function writeAllNewMessagesToChat() {
 //--------------------------------------------
 //do not touch anything above this line
 const possibleCommands = Object.freeze({
-  NOOP: Symbol(''),
-  AHOJ: Symbol('ahoj'),
-  SOUTH: Symbol('south'),
-  NORTH: Symbol('north'),
-  WEST: Symbol('west'),
-  EAST: Symbol('east'),
-  JMENO: Symbol('jmeno'),
+  NOOP: Symbol(""),
+  AHOJ: Symbol("ahoj"),
+  SOUTH: Symbol("south"),
+  NORTH: Symbol("north"),
+  WEST: Symbol("west"),
+  EAST: Symbol("east"),
+  JMENO: Symbol("jmeno"),
 });
 
 const commandAliases = {
-  NOOP: [''],
-  AHOJ: ['cau', 'hi', 'zdar', 'cauko'],
-  NORTH: ['n', 'sever'],
-  SOUTH: ['s', 'jih'],
-  EAST: ['e', 'vychod'],
-  JMENO: ['jmeno', 'me jmeno', 'jake je me jmeno'],
-}
+  NOOP: [""],
+  AHOJ: ["cau", "hi", "zdar", "cauko"],
+  NORTH: ["n", "sever"],
+  SOUTH: ["s", "jih"],
+  EAST: ["e", "vychod"],
+  JMENO: ["jmeno", "me jmeno", "jake je me jmeno"],
+};
 
 const items = {
-	shovel: {
-  	name: '',
-    description: 'Tohle je moc hezka lopata, akorat s ni nekoho jebout po palici'
-  }
-}
+  shovel: {
+    name: "",
+    description:
+      "Tohle je moc hezka lopata, akorat s ni nekoho jebout po palici",
+  },
+};
 
 const gameMap = {
-	hotel_lobby: {
-  	name: "Vstupni hala",
+  hotel_lobby: {
+    name: "Vstupni hala",
     description: "Prave se nachazite v hale temné hotelu",
     items: [items.shovel],
-    entities: ['Lucie'],
+    entities: ["Lucie"],
     connects: [
-    	{link: 'stairs', type: 'hidden', direction: 'north'},
-      {link: 'mistnost2', type: 'hidden', direction: 'down'}
-     ],
-    state: ['normal'],
-    actions: ['ahoj', 'dig', 'talk'],
-    quests: [{
-    	id: 'jmeno',
-      name: 'Predstav se',
-      state: ['unsolved']
-    }]
-  }
+      { link: "stairs", type: "hidden", direction: "north" },
+      { link: "mistnost2", type: "hidden", direction: "down" },
+    ],
+    state: ["normal"],
+    actions: ["ahoj", "dig", "talk"],
+    quests: [
+      {
+        id: "jmeno",
+        name: "Predstav se",
+        state: ["unsolved"],
+      },
+    ],
+  },
 };
 
 const userProfile = {
@@ -190,25 +194,23 @@ var gameState = {
   currentPosition: 0,
   history: [],
   inventory: {},
-  userProfile: userProfile
-}
+  userProfile: userProfile,
+};
 
 function getResponse() {
   const lastCommand = gameState.history[gameState.history.length - 1];
   const lastMessage = gameState.history[gameState.history.length - 2];
-  
+
   if (lastCommand.data.name == possibleCommands.AHOJ.description) {
-    if (userProfile.name === null)
-      return `ahoj, jake je tve jmeno?`;
-    else
-      return 'ahoj';
+    if (userProfile.name === null) return `ahoj, jake je tve jmeno?`;
+    else return "ahoj";
   }
-  
+
   if (lastMessage.text == "ahoj, jake je tve jmeno?") {
     gameState.userProfile.name = lastCommand.text;
     return `Krásné jméno, odteď si budu pamatovat, že se jmenuješ ${lastCommand.text}`;
   }
-  
+
   if (lastCommand.data.name == possibleCommands.JMENO.description) {
     return `Řekl jsi mi, že se jmenuješ ${gameState.userProfile.name}`;
   }
